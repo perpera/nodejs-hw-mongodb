6b1338ff602821d8ebb9c99b53887767512b6543
perpera <poalina@icloud.com>
2025-03-08 16:19:35 +0200

    throw error fix

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -50,8 +50,7 @@ export const patchContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.send({
@@ -67,8 +66,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
