f0199e834a5113905c3419310670bd2f19146c8c
perpera <poalina@icloud.com>
2025-04-06 17:35:16 +0300

    initial swagger

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import {UPLOAD_DIR} from './constants/index.js';
+import {swaggerDocs} from './middlewares/swaggerDocs.js';
 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -34,6 +35,7 @@ export const setupServer = () => {
   app.use(errorHandler);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
